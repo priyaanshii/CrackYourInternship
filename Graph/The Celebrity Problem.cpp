8b6d1cc88572a31bc8cb10af1a6a1bf47f738315
priyanshi huria <91135853+priyaanshii@users.noreply.github.com>
2022-07-05 14:17:03 +0530

    Create The Celebrity Problem.cpp

---
+++ Graph/The Celebrity Problem.cpp
@@ -0,0 +1,35 @@
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        // code here 
+        vector<int>indegree(n+1,0);
+        vector<int>outdegree(n+1,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(M[i][j]==1)
+                {
+                    indegree[j]++;
+                    outdegree[i]++;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==n-1 && outdegree[i]==0)
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
